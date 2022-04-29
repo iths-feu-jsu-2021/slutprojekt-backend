ba995ca9873820246ea57f88aba97ca0a8283ade
Markus <markus.mork@iths.se>
2022-04-29 10:57:53 +0200

    Add seed function

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const db = require('../database/connection')
 const {DataTypes} = require('sequelize')
+
+// class User extends Model {}
 const User = db.define('User', {
     role:{
         type: DataTypes.ENUM("worker", "customer"),
