395eea6041e52c9ee7b19e7cb6ae91636876e041
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-05-02 09:50:55 +0200

    lite kod på fel branch

---
+++ controllers/accountController.js
@@ -0,0 +1,9 @@
+const User = require('../models/User')
+
+module.exports = {
+
+    create: async (req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,9 @@
-const controllers = require('../controllers')
+const accountController = ('./accountController.js')
+const { Router} = require('express')
+
+const router = new Router
+
+router.post('/register', accountController.create)
+router.post('/login', accountController.login)
+
 
