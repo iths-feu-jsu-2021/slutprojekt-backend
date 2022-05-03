84bcafec3aa8bab8a38958996339bae06381a8cd
Markus <markus.mork@iths.se>
2022-05-03 16:02:30 +0200

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const accountController = require('../controllers/accountController')
 const matterController = require('../controllers/matterController')
 const matterValidator = require('../validations/matterValidator')
+const messageValidator = require('../validations/messageValidator')
 const { Router } = require('express')
 const res = require('express/lib/response')
 
@@ -12,4 +13,5 @@ router.get('/', () =>{
     res.json('server funkar')
 })
 router.post('/matter', matterValidator.checkRole, matterController.create )
+router.post('/message', messageValidator.checkRole, messageController.create )
 module.exports = router
